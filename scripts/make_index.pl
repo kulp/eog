58bb5ea89d67ff29c91bb93c5d2a82985b33626c
Darren Kulp <darren@kulp.ch>
2011-08-22 17:52:28 -0500

    Use CGI to output HTML

--- scripts/make_index.pl
+++ scripts/make_index.pl
@@ -2,6 +2,7 @@
 use strict;
 use warnings;
 
+use CGI qw(:standard); # just for HTML shortcuts
 use List::MoreUtils qw(uniq);
 use Perl6::Slurp qw(slurp);
 
@@ -27,74 +28,66 @@ my %vcount   = map { $_ => scalar @{ $variants{$_} } } keys %variants;
 
 my $headcontent = eval { slurp("scripts/kulpheadcontent") } || "";
 
-print qq(
-<!DOCTYPE HTML SYSTEM>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Echoes of Grace layout project</title>
-<script type="text/javascript" src="scripts/sorttable.js"></script>
-<script type="text/javascript" src="scripts/kulpstuff.js"></script>
-<script type="text/javascript" src="scripts/table.js"> </script>
-<link rel="stylesheet" type="text/css" href="scripts/main.css" />
-$headcontent
-</head>
-<body onLoad="document.getElementById('searchbox').focus()">
-<p>Download a <a href="EOG_midi_pdf.zip">zip file of all PDF and MIDI files</a> listed.</p>
-<table class="sortable" id="main">
-  <thead>
-    <tr>
-      <th class="corner">Filter:</th>
-      <th class="sortbox" colspan="4">
-        <form onsubmit="return false;">
-          <input id="searchbox" name="filter" onkeyup="filter2(this, 'main')" type="text" size="35">
-          <button onclick="return do_reset();" value="Reset">Reset</button>
-        </form>
-      </th>),
- (map qq(
-      <th class="wide" colspan="$vcount{$_}">$_</th>), @dirs),
-      qq(
-    </tr>
-    <tr class="realhead">
-      <th>Index</th><th>Title</th><th>Poet</th><th>Composer</th><th>Meter</th>),
- (map qq(
-      <th class="sorttable_nosort">$_</th>), map @{ $variants{$_} }, @dirs),
-      qq(
-    </tr>
-  </thead>
-  <tbody>);
+print
+    start_html(-title  => "Echoes of Grace layout project",
+           -style  => { -src => "scripts/main.css" },
+           -head   => [ meta({ -http_equiv => "Content-Type",
+                             -content => "text/html; charset=UTF-8" }),
+                        $headcontent ],
+           -script => [ { -src => "scripts/sorttable.js" },
+                        { -src => "scripts/kulpstuff.js" },
+                        { -src => "scripts/table.js"     }, ],
+           -onLoad => "document.getElementById('searchbox').focus()",
+       ),
 
-for my $stem (@stems) {
-    my $src      = "src/$stem.ly";
+    p("Download a", a({ -href => "EOG_midi_pdf.zip" }, "zip file of all PDF and MIDI files"), "listed."),
+    table({ -class => "sortable", -id => "main" },
+        thead(
+            Tr(
+                th({ -class => "corner" }, "Filter:"),
+                th({ -class => "sortbox", -colspan => 4 },
+                    start_form({ -onSubmit => "return false;", -action => "" }),
+                        input({ -id => "searchbox",
+                                -name => "filter",
+                                -onkeyup => "filter2(this, 'main')",
+                                -type => "text",
+                                -size => 35 }),
+                        button({ -onclick => "return do_reset();", -value => "Reset" }, "Reset"),
+                    end_form,
+                ),
 
-    my $title    = get_key $src => "title";
-    my $poet     = get_key $src => "poet";
-    my $composer = get_key $src => "composer";
-    my $meter    = get_key $src => "meter";
+             (map th({ -class => "wide", -colspan => $vcount{$_} }, $_), @dirs),
+                Tr({ -class => "realhead" },
+                    th("Index"), th("Title"), th("Poet"), th("Composer"), th("Meter"),
+                    (map th({ -class => "sorttable_nosort" }, $_), map @{ $variants{$_} }, @dirs),
+                ),
+            ),
+        ),
+        tbody(
+            (map {
+                my $stem = $_;
+                my $src      = "src/$stem.ly";
 
-    my $index    = int(($stem =~ /EOG(\d+)/)[0]);
-    (my $safetitle = $title) =~ s/[^\s\w]//g;
-    print qq(
-    <tr>
-      <th class="index">$index</th>
-      <td class="title" sorttable_customkey="$safetitle">$title</td>
-      <td class="poet">$poet</td>
-      <td class="composer">$composer</td>
-      <td class="meter">$meter</td>);
-    for my $dir (@dirs) {
-        for my $v (@{ $variants{$dir} }) {
-            print qq(
-      <td><a href="$dir/$v/$stem.$exts{$dir}">$stem.$exts{$dir}</a></td>);
-        }
-    }
-    print qq(
-    </tr>);
-}
+                my $title    = get_key $src => "title";
+                my $poet     = get_key $src => "poet";
+                my $composer = get_key $src => "composer";
+                my $meter    = get_key $src => "meter";
 
-print qq(
-  </tbody>
-</table>
-</body>
-</html>
-);
+                my $index    = int(($stem =~ /EOG(\d+)/)[0]);
+                (my $safetitle = $title) =~ s/[^\s\w]//g;
+
+                Tr(
+                    th({ -class => "index"                                     }, $index),
+                    td({ -class => "title", -sorttable_customkey => $safetitle }, $title),
+                    td({ -class => "poet"                                      }, $poet),
+                    td({ -class => "composer"                                  }, $composer),
+                    td({ -class => "meter"                                     }, $meter),
+                    (map { my $dir = $_; map {
+                        td([ a({ -href => "$dir/$_/$stem.$exts{$dir}" }, "$stem.$exts{$dir}") ]),
+                    } @{ $variants{$dir} } } @dirs),
+                );
+            } @stems),
+            ),
+        ),
+    end_html;
 
