c69a08a4a80b93b429ffc2e9eb5c08bee7ae35a9
Darren Kulp <darren@kulp.ch>
2016-01-30 16:59:01 -0700

    Adding sync.sh script for S3 upload

---
+++ scripts/sync.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+S3DEST="s3://eog.kulp.ch"
+S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store"
+s3cmd $S3OPTS sync *.html *.zip $S3DEST
+for dir in scripts MP3 TXT PDF
+do
+    s3cmd $S3OPTS sync $dir/ $S3DEST/$dir/ &
+done
+wait
+
