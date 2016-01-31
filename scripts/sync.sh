f6805246d012b45a799be49b790c2cd6df2ff406
Darren Kulp <darren@kulp.ch>
2016-01-30 17:22:33 -0700

    Add forgotten MIDIs to upload

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 S3DEST="s3://eog.kulp.ch"
 S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store"
 s3cmd $S3OPTS sync *.html *.zip $S3DEST
-for dir in scripts MP3 TXT PDF
+for dir in scripts MP3 TXT PDF MIDI
 do
     s3cmd $S3OPTS sync $dir/ $S3DEST/$dir/ &
 done
