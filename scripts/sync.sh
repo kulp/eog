bc1afa9e4373a3057a5241c99d0688c114d4b2ff
Darren Kulp <darren@kulp.ch>
2016-02-20 12:04:04 -0700

    Upload .ZIP in parallel to others

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 S3DEST="s3://eog.kulp.ch"
 S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store"
-s3cmd $S3OPTS sync *.html *.zip $S3DEST
+s3cmd $S3OPTS sync *.html *.zip $S3DEST &
 for dir in scripts MP3 TXT PDF MIDI
 do
     s3cmd $S3OPTS sync $dir/ $S3DEST/$dir/ &
