7718c71f3128910facf2619bd9b47f8639667aa5
Darren Kulp <darren@kulp.ch>
2016-03-15 20:08:35 -0700

    Set encoding to UTF-8 explicitly
    Now TXT files will have 'Content-Type: text/plain; charset=UTF-8' as a
    header, ensuring proper display.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 S3DEST="s3://eog.kulp.ch"
-S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store"
+S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store --encoding=UTF-8 --add-encoding-exts=txt,js,css,html,ly"
 s3cmd $S3OPTS sync *.html *.zip $S3DEST &
 for dir in scripts MP3 TXT PDF MIDI
 do
