65cee9ea3307c1dbb1e414d6dbbc1d8cc81bf5ac
Darren Kulp <darren@kulp.ch>
2016-12-23 15:29:09 -0800

    Track uploads

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 S3DEST="s3://eog.kulp.ch"
 S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store --encoding=UTF-8 --add-encoding-exts=txt,js,css,html,ly,m3u"
+before=refs/upload/began-$(date +%s)
+git update-ref $before HEAD
 s3cmd $S3OPTS sync *.m3u *.zip $S3DEST &
 for dir in scripts MP3 TXT PDF MIDI
 do
@@ -10,4 +12,8 @@ wait # upload all music files before we update index
 # update index last, in case we fail
 s3cmd $S3OPTS sync *.html $S3DEST &
 wait
+after=refs/upload/finished-$(date +%s)
+git update-ref $after HEAD
+git update-ref refs/upload/finished-latest $after
+git update-ref -d $before
 
