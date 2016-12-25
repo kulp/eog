d4a4572bad1cee4886854b00cdd14d7101339f75
Darren Kulp <darren@kulp.ch>
2016-12-25 08:32:16 -0800

    Stop publishing .zip file

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,14 +3,13 @@ S3DEST="s3://eog.kulp.ch"
 S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store --encoding=UTF-8 --add-encoding-exts=txt,js,css,html,ly,m3u"
 before=refs/upload/began-$(date +%s)
 git update-ref $before HEAD
-s3cmd $S3OPTS sync *.m3u *.zip $S3DEST &
 for dir in scripts MP3 TXT PDF MIDI
 do
     s3cmd $S3OPTS sync $dir/ $S3DEST/$dir/ &
 done
 wait # upload all music files before we update index
 # update index last, in case we fail
-s3cmd $S3OPTS sync *.html $S3DEST &
+s3cmd $S3OPTS sync *.m3u *.html $S3DEST &
 wait
 after=refs/upload/finished-$(date +%s)
 git update-ref $after HEAD
