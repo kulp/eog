81c174959fd6d2898552b2315575a75d94749309
Darren Kulp <darren@kulp.ch>
2017-05-02 19:10:23 -0700

    Replace `s3cmd` invocations with one `aws s3`

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 S3DEST="s3://eog.kulp.ch"
-S3OPTS="--acl-public --reduced-redundancy --guess-mime-type --no-preserve --recursive --exclude=.DS_Store --encoding=UTF-8 --add-encoding-exts=txt,js,css,html,ly,m3u"
+S3OPTS="--acl public-read --storage-class REDUCED_REDUNDANCY --exclude .DS_Store --content-encoding UTF-8"
 before=refs/upload/began-$(date +%s)
 git update-ref $before HEAD
-for dir in scripts MP3 TXT PDF MIDI
-do
-    s3cmd $S3OPTS sync $dir/ $S3DEST/$dir/ &
-done
-wait # upload all music files before we update index
-# update index last, in case we fail
-s3cmd $S3OPTS sync *.m3u *.html $S3DEST &
-wait
+aws s3 sync $S3OPTS \
+    --exclude "*" \
+    --include "MIDI/*/*.midi" \
+    --include "MP3/*/*.mp3" \
+    --include "PDF/*/*.pdf" \
+    --include "TXT/*/*.txt" \
+    --include "scripts/*" \
+    --include "*.m3u" \
+    --include "index.html" \
+    . $S3DEST/
 after=refs/upload/finished-$(date +%s)
 git update-ref $after HEAD
 git update-ref refs/upload/finished-latest $after
