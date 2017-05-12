f0433fb63eb5de9fdf96e4d40441dde49a658148
Darren Kulp <darren@kulp.ch>
2017-05-12 07:29:36 -0700

    Correct encoding for text files

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 S3DEST="s3://eog.kulp.ch"
-S3OPTS="--acl public-read --storage-class REDUCED_REDUNDANCY --exclude .DS_Store --content-encoding UTF-8"
+S3OPTS="--acl public-read --storage-class REDUCED_REDUNDANCY --exclude .DS_Store"
 before=refs/upload/began-$(date +%s)
 git update-ref $before HEAD
 aws s3 sync $S3OPTS \
@@ -8,9 +8,15 @@ aws s3 sync $S3OPTS \
     --include "MIDI/*/*.midi" \
     --include "MP3/*/*.mp3" \
     --include "PDF/*/*.pdf" \
-    --include "TXT/*/*.txt" \
     --include "scripts/*" \
+    . $S3DEST/
+aws s3 sync $S3OPTS --content-type "text/plain; charset=utf-8" \
+    --exclude "*" \
+    --include "TXT/*/*.txt" \
     --include "*.m3u" \
+    . $S3DEST/
+aws s3 sync $S3OPTS --content-type "text/html; charset=utf-8" \
+    --exclude "*" \
     --include "index.html" \
     . $S3DEST/
 after=refs/upload/finished-$(date +%s)
