7ab973e723a6b39bf63a96c11436e6b4a2498a13
Darren Kulp <darren@kulp.ch>
2017-06-11 07:42:22 -0700

    Add rudimentary linebreaking to metrical format

--- scripts/format_metrical_index.pl
+++ scripts/format_metrical_index.pl
@@ -2,22 +2,34 @@
 use strict;
 
 my $open = 0;
+my $closeright = 0;
 
 while (<>) {
     chomp;
     next if /^$/;
     if (/^(?! )/) {
         # meter
-        print qq(\\end{flushright}\n) if $open;
-        print qq(\\needspace{2\\baselineskip}\n);
-        print qq(\\center\\textbf{$_}\n);
-        print qq(\\begin{flushright}\n);
+        print qq(\\end{flushright}\n) if $open and $closeright;
+        print qq(\\needspace{\\baselineskip}\n);
+        print qq({\\centering\\textbf{$_}\\par}\n);
         $open = 1;
+        $closeright = 0;
     } else {
+        if (/^ (Also )?Tunes?/) {
+            print qq(\\begin{flushright});
+            $closeright = 1;
+        }
         my ($name, $nums) = split /\t/;
-        print qq($name\\dotfill $nums \\\\\n);
+        if (length $name > 20) {
+            my @words = split " ", $name;
+            my @first = @words[0 .. $#words / 2];
+            my @last  = @words[$#words / 2 + 1 .. $#words];
+            print qq({\\flushleft @first \\\\ @last\\dotfill }$nums\n);
+        } else {
+            print qq(\n$name\\dotfill $nums\n);
+        }
     }
 }
 
-print qq(\\end{flushright}\n) if $open;
+print qq(\\end{flushright}\n) if $open and $closeright;
 
