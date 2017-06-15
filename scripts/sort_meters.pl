a83bbb2f97dfb0ca2f3ad409072dd255fc2288f7
Darren Kulp <darren@kulp.ch>
2017-06-15 06:36:13 -0700

    Order `with Refrain` better

--- scripts/sort_meters.pl
+++ scripts/sort_meters.pl
@@ -3,6 +3,8 @@ use strict;
 
 sub normalize {
     local $_ = shift;
+    chomp;
+    s/\t.*$//;
     if (my ($mult, $base, $rest) = /(\d+)-(\d+)s\.(?:\s*(.*)\s*)?/) {
         return ("$base. " x $mult) . $rest;
     } elsif (my ($base, $dup) = /((?:\d+\. )+)D\./) {
@@ -37,7 +39,12 @@ sub by_meter {
 
     if (is_num($aa)) {
         if (is_num($bb)) {
-            return compare_recurse [ split " ", $aa ], [ split " ", $bb ];
+            my ($an) = $aa =~ /^(.*?)\s*(with Refrain)?$/;
+            my ($bn) = $bb =~ /^(.*?)\s*(with Refrain)?$/;
+            return
+                ($an eq $bn)
+                  ? ($aa =~ /Refrain/) - ($bb =~ /Refrain/)
+                  : compare_recurse [ split " ", $an ], [ split " ", $bn ];
         } else {
             return 1;
         }
@@ -55,8 +62,6 @@ sub by_meter {
         }
     }
 }
-
-#print normalize shift;
 
 print for sort by_meter <>;
 
