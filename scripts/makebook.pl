a4287b54fdc6af5caa95af9fca0b36c1f5db986f
Darren Kulp <darren@kulp.ch>
2017-07-21 06:04:56 -0700

    Return to not clipping full-page hymns

--- scripts/makebook.pl
+++ scripts/makebook.pl
@@ -52,8 +52,8 @@ for my $pdf (@ARGV) {
         my $hyper = $page > 1 ? "" : "\\hypertarget{$name}";
         print qq(\\vfill\\Large{\\textsc{Additional Tunes}}\\vfill\n)
             if $name =~ /EOGa/ and $prev_name !~ /EOGa/;
-        printf q(%-20s{\\includegraphics[scale=%4.3f,clip=true,trim=%2dpt %3dpt %2dpt %2dpt,page=%d]{%s}} \\\\)."\n",
-               $hyper, $scale, $crop_amount_left, $crop_amount_bottom, $crop_amount_right, $crop_amount_top, $page, $basename;
+        printf q(%-20s{\\includegraphics[scale=%4.3f,clip=%5s,trim=%2dpt %3dpt %2dpt %2dpt,page=%d]{%s}} \\\\)."\n",
+               $hyper, $scale, $clip, $crop_amount_left, $crop_amount_bottom, $crop_amount_right, $crop_amount_top, $page, $basename;
         $prev_name = $name;
     }
 }
