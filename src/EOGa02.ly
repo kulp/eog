kulp/eog

commit 849ff1062eb2a7e0abf0871cd51cae1ef2359912

Normalise additional tunes

src/EOGa02.ly
@@ -1,6 +1,6 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 2) (#t #f))
   system-system-spacing.padding = 11
@@ -8,10 +8,10 @@
 
 \header{
   hymnnumber = "2"
-  title = "German Evening Hymn"
-  tunename = ""
+  tunename = "German Evening Hymn"
   meter = "8. 7. 8. 7."
   composer = "German"
+  alternate = \markup { "Alternate tune for No. 286." }
   tagline = ##f
 }
 
@@ -95,8 +95,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 152." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa01.ly
@@ -1,16 +1,19 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 2) (#t #f))
 }
 
 \header{
   hymnnumber = "1"
-  title = "French"
-  tunename = ""
+  tunename = "French"
   meter = "C. M."
   composer = "Scottish Psalter, 1615"
+  alternate = \markup { \column {
+    "Alternate tune for No. 286."
+    "See also No. 275, Little Flock."
+  } }
   tagline = ##f
 }
 
@@ -97,9 +100,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 286." } }
-\noPageBreak \markup { \fill-line { "See also No. 275, Little Flock." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa03.ly
@@ -1,6 +1,6 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 3) (#t #f))
   system-system-spacing.padding = 11
@@ -8,10 +8,10 @@
 
 \header{
   hymnnumber = "3"
-  title = "Hail, Sovereign Love"
   tunename = "Cyprus"
   meter = "L. M."
   composer = "Lowell Mason"
+  alternate = \markup { "Alternate tune for No. 88." }
   tagline = ##f
 }
 
@@ -99,8 +99,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 88." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa04.ly
@@ -1,16 +1,16 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 4) (#t #f))
 }
 
 \header{
   hymnnumber = "4"
-  title = "Saved by Grace"
-  tunename = ""
+  tunename = "Saved by Grace"
   meter = "L. M. D."
   composer = "Geo. C. Stebbins"
+  alternate = \markup { "Alternate tune for No. 144." }
   tagline = ##f
 }
 
@@ -117,8 +117,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 144." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa05.ly
@@ -1,16 +1,16 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 2) (#t #f))
 }
 
 \header{
   hymnnumber = "5"
-  title = "Olive’s Brow"
-  tunename = ""
+  tunename = "Olive’s Brow"
   meter = "P. M."
   composer = "W. B. Bradbury"
+  alternate = \markup { "Alternate tune for No. 205." }
   tagline = ##f
 }
 
@@ -98,8 +98,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 205." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa06.ly
@@ -1,6 +1,6 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   ragged-last = ##t
   % shrink space before markup verses to fit next hymn on same page
@@ -10,10 +10,13 @@
 
 \header{
   hymnnumber = "6"
-  title = "Luther"
-  tunename = ""
+  tunename = "Luther"
   meter = "8. 7. 8. 7. 8. 8. 7."
   composer = "Geistliche Lieder, 1535"
+  alternate = \markup { \column {
+    "Alternate tune for No. 281."
+    "See also No. 188, Little Flock."
+  } }
   tagline = ##f
 }
 
@@ -113,9 +116,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 281." } }
-\noPageBreak \markup { \fill-line { "See also No. 188, Little Flock." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa07.ly
@@ -1,6 +1,6 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   ragged-last-bottom = ##t
   ragged-last = ##t
   % shrink space before markup verses to fit previous hymn on same page
@@ -10,10 +10,10 @@
 
 \header{
   hymnnumber = "7"
-  title = "Martyn"
-  tunename = ""
+  tunename = "Martyn"
   meter = "8-7s."
   composer = "S. B. Marsh"
+  alternate = \markup { "Alternate tune for No. 379." }
   tagline = ##f
 }
 
@@ -105,8 +105,6 @@ notesBass = {
     \include "common/midi.ily"
   }
 }
-
-\noPageBreak \markup { \fill-line { "Alternate tune for No. 379." } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.
 

src/EOGa08.ly
@@ -1,6 +1,6 @@
 \include "common/global.ily"
 \paper {
-  \include "common/paper.ily"
+  \include "common/paper-additional.ily"
   markup-system-spacing.padding = 0
   markup-system-spacing.basic-distance = 0
   scoreTitleMarkup = \markup {
@@ -11,7 +11,7 @@
         \fill-line {
           \huge \larger \larger \bold {
             \concat { "  " \fromproperty #'header:hymnnumber }
-            \mytitle
+            \fromproperty #'header:title
             \null
           }
         }

commit a346a3d64d52f2db36161de7e23a4391b452c345

Correct error introduced in 849ff10
Alternate tune for EOGa08 was corrupted in 849ff10, now mended.

src/EOGa02.ly
@@ -10,7 +10,7 @@
   tunename = "German Evening Hymn"
   meter = "8. 7. 8. 7."
   composer = "German"
-  alternate = \markup { "Alternate tune for No. 286." }
+  alternate = \markup { "Alternate tune for No. 152." }
   tagline = ##f
 }
 