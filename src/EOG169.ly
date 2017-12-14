kulp/eog

commit d924cbcbde6247ec83986179d8c26cd94b8cc2eb

General vspace review

src/EOG169.ly
@@ -2,9 +2,7 @@
 \paper {
   \include "common/paper.ily"
   ragged-last-bottom = ##t
-  systems-per-page = ##f
   system-count = #(cond (is-eogsized 2) (#t #f))
-  system-system-spacing.padding = 11
 }
 
 \header{
@@ -164,16 +162,14 @@ wordsF = \markuplist {
   } #}))
 }
 
-\markup { \vcenter \column {
-  \vspace #1
-  \fill-line {
-    \null
-    \line{ \bold 5 \column { \wordsE } }
-    \null
+\markup { \column {
+  \fill-line { \vcenter \column {
+    \vspace #1
+    \line{ \bold 5 \column { \wordsE } } \combine \null \vspace #0.4
     \line{ \bold 6 \column { \wordsF } }
-    \null
-  }
-  \vspace #1
+    \vspace #1
+  } }
+  \fill-line { \line { Alternate tune: No. 44. } }
 } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.

src/EOG046.ly
@@ -4,8 +4,6 @@
   ragged-bottom = ##t
   page-count = #(cond (is-eogsized 2) (#t #f))
   systems-per-page = #(cond (is-eogsized 1) (#t #f))
-  score-markup-spacing.basic-distance = 3
-  score-markup-spacing.padding = 3
 }
 
 \header{
@@ -179,12 +177,16 @@ wordsF = \markuplist {
 
 \noPageBreak
 
-\markup { \fill-line {
-  \null
-  \line{ \bold 5 \column { \wordsE } }
-  \null
-  \line{ \bold 6 \column { \wordsF } }
-  \null
+\markup { \vcenter \column {
+  \vspace #1
+  \fill-line {
+    \null
+    \line{ \bold 5 \column { \wordsE } }
+    \null
+    \line{ \bold 6 \column { \wordsF } }
+    \null
+  }
+  \vspace #1
 } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.

src/EOG097.ly
@@ -3,8 +3,8 @@
   \include "common/paper.ily"
   ragged-bottom = ##t
   system-count = #(cond (is-eogsized 2) (#t #f))
-  system-system-spacing.padding = 1
-  system-system-spacing.basic-distance = 1
+  system-system-spacing.padding = 3
+  system-system-spacing.basic-distance = 3
 }
 
 \header{
@@ -164,7 +164,7 @@ wordsE = \markuplist {
 }
 
 \markup { \vcenter \column {
-  \vspace #1
+  \vspace #0.5 % not enough space for \vspace #1
   \fill-line {
     \null
     \line{ \bold 4 \column { \wordsD } }
@@ -172,7 +172,7 @@ wordsE = \markuplist {
     \line{ \bold 5 \column { \wordsE } }
     \null
   }
-  \vspace #1
+  \vspace #0.5 % not enough space for \vspace #1
   \fill-line { \line { Alternate tune: No. 288. } }
 } }
 

src/EOG143.ly
@@ -3,7 +3,6 @@
   \include "common/paper.ily"
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 2) (#t #f))
-  system-system-spacing.padding = 11
 }
 
 \header{
@@ -155,16 +154,13 @@ wordsE = \markuplist {
   } #}))
 }
 
-\markup { \vcenter \column {
-  \vspace #1
-  \fill-line {
-    \null
-    \line{ \bold 4 \column { \wordsD } }
-    \null
+\markup { \column {
+  \fill-line { \vcenter \column {
+    \vspace #1
+    \line{ \bold 4 \column { \wordsD } } \combine \null \vspace #0.4
     \line{ \bold 5 \column { \wordsE } }
-    \null
-  }
-  \vspace #1
+    \vspace #1
+  } }
   \fill-line { \line { Alternate tune: No. 252. } }
 } }
 

src/EOG150.ly
@@ -2,6 +2,8 @@
 \paper {
   \include "common/paper.ily"
   ragged-bottom = ##t
+  system-system-spacing.padding = 3
+  system-system-spacing.basic-distance = 3
 }
 
 \header{

src/EOG151.ly
@@ -2,7 +2,8 @@
 \paper {
   \include "common/paper.ily"
   ragged-last-bottom = ##t
-  systems-per-page = ##f
+  system-system-spacing.padding = 3
+  system-system-spacing.basic-distance = 3
 }
 
 \header{

src/EOG168.ly
@@ -4,6 +4,8 @@
   ragged-last-bottom = ##t
   system-count = #(cond (is-eogsized 5) (#t #f))
   systems-per-page = #(cond (is-eogsized 4) (#t #f))
+  score-markup-spacing.padding = 2
+  score-markup-spacing.basic-distance = 2
 }
 
 \header{

src/EOG347.ly
@@ -2,6 +2,8 @@
 \paper {
   \include "common/paper.ily"
   ragged-last-bottom = ##t
+  system-system-spacing.padding = 3
+  system-system-spacing.basic-distance = 3
 }
 
 \header{

commit 5e937596963af4259a833ee2bd203c191203c713

Correct erroneous change in d924cbcbd
Commit d924cbcbd introduced a spurious alternate tune by a copy-paste
error.

src/EOG169.ly
@@ -167,9 +167,7 @@ wordsF = \markuplist {
     \vspace #1
     \line{ \bold 5 \column { \wordsE } } \combine \null \vspace #0.4
     \line{ \bold 6 \column { \wordsF } }
-    \vspace #1
   } }
-  \fill-line { \line { Alternate tune: No. 44. } }
 } }
 
 \version "2.19.49"  % necessary for upgrading to future LilyPond versions.