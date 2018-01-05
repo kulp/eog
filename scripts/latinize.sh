3d43a90537cb555047383a3a2e553be42eb3cca2
Darren Kulp <darren@kulp.ch>
2018-01-05 05:58:20 -0700

    Add back spaces around dashes in latinized lyrics
    When an em- or en-dash is replaced by a hyphen, it becomes necessary to
    set it off with spaces for clarity. This also aids in comparing
    latinized sources with text extracted from the EGHB app.

--- scripts/latinize.sh
+++ scripts/latinize.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-tr '–—‘’“”' "--''"'""' < $1 | iconv -c --from-code=UTF-8 --to-code=latin1
+# put spaces around dashes when they become hyphens
+sed 's/[–—]/ - /g' $1 |
+# convert smart quotes to regular ones
+tr '‘’“”' "''"'""' |
+# change encoding to latin-1
+iconv -c --from-code=UTF-8 --to-code=latin1
